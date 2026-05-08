1ef8055664134eedfa6da8b8a6e43b4327bef541
Zetsuki <zetsukiog@gmail.com>
2026-05-08 13:22:01 +0200

    Vue index comptes finie avec mock ups. TODO: création compte et modification. Maj données sur vue index

--- SousMalins/wwwroot/js/site.js
+++ SousMalins/wwwroot/js/site.js
@@ -1,1 +1,28 @@
-﻿
+﻿// Table datatable
+$(document).ready(function () {
+
+    $('.datatable').each(function () {
+
+        // évite double init
+        if (!$.fn.DataTable.isDataTable(this)) {
+
+            $(this).DataTable({
+                language: {
+                    search: "Rechercher :",
+                    lengthMenu: "Afficher _MENU_ lignes",
+                    info: "Page _PAGE_ sur _PAGES_",
+                    paginate: {
+                        previous: "Précédent",
+                        next: "Suivant"
+                    },
+                    zeroRecords: "Aucun résultat trouvé"
+                },
+                pageLength: 10,
+                responsive: true
+            });
+
+        }
+
+    });
+
+});
